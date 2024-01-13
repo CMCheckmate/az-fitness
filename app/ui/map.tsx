81b85864c911a5f641c1f347d232e22f60dcb47d
CMCheckmate <mingraygoy@gmail.com>
2024-01-14 05:00:10 +1300

    Fixing map

--- app/ui/map.tsx
+++ app/ui/map.tsx
@@ -2,6 +2,6 @@ export default function Map() {
     const center = { lat: -36.7167, lng: 174.7000 };
 
     return (
-        <iframe src={`http://maps.google.com/maps?q=${center.lat},${center.lng}&z=15&output=embed`} className='w-full aspect-[5/2]'></iframe>
+        <iframe src={`http://maps.google.com/maps?q=${center.lat},${center.lng}&z=15&output=embed`} className='w-full aspect-[5/2]' loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
     );
 }
